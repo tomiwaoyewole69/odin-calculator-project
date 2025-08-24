c88ba6bb299789daf80ade6dab418d13ef1ed604
Tomiwa Oyewole <221529936+Tomiwaoyewole69@users.noreply.github.com>
2025-08-24 02:48:07 +0100

    Added the functionality for the calculation

--- script.js
+++ script.js
@@ -12,24 +12,26 @@ function divide(num1, num2) {
 }
 
 let firstNum = 3;
-let operator = Number('+')
+let operator = '-'
 let secondNum = 5;
 
 function operate(operator, num1, num2) {
   switch (operator) {
     case '+':
-      add(num1, num2)
-      break
+      console.log(add(num1, num2))
+      break;
     case '-':
-      subtract(num1, num2)
+      console.log(subtract(num1, num2))
       break
     case '*':
-      multiply(num1, num2)
+      console.log(multiply(num1, num2))
       break
     case '/':
-      divide(num1, num2)
+      console.log(divide(num1, num2))
+      break
+    default:
       break
   }
 }
 
-console.log(operate(operator, firstNum, secondNum))
+operate(operator, firstNum, secondNum)
