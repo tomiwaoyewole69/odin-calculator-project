a87d0092adef4e829257b5d3100fe9d63962fe2d
Tomiwa Oyewole <221529936+tomiwaoyewole69@users.noreply.github.com>
2025-08-28 16:52:24 +0100

    Added new buttons with functionality and option to enter number with keyboard support

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let resultDisplay = document.querySelector(".input");
 const buttons = document.querySelectorAll("button");
 
-const operators = ["+", "-", "*", "/"];
+const operators = ["+", "-", "*", "/", "=", "DEL", "C", "%"];
 let firstNum = "";
 let operator = "";
 let secondNum = "";
@@ -39,7 +39,7 @@ function clearAll() {
   operator = "";
   secondNum = "";
 }
-
+// function to evaluate the expression
 function calculateResult() {
   secondNum = Number(resultDisplay.value);
   const calculate = operate(operator, firstNum, secondNum);
@@ -50,23 +50,66 @@ function calculateResult() {
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const value = button.value;
+    // check if the value is in the operator array on button click
     if (operators.includes(value)) {
-      if (resultDisplay.value) {
-        operator = value;
-        firstNum = Number(resultDisplay.value);
+      if (value == "=") {
+        // evaluate the equation
+        if (resultDisplay.value == "" || (operator == "/" && secondNum == 0)) {
+          resultDisplay.value = "Error";
+        } else if (firstNum !== "" && operator !== "") {
+          calculateResult();
+        }
+      } else if (value == "C") {
+        // clear the display value
         resultDisplay.value = "";
+        clearAll();
+      } else if (value == "DEL") {
+        // delete each value
+        resultDisplay.value = resultDisplay.value.slice(0, -1);
+      } else if (value == "%") {
+        resultDisplay.value = Number(resultDisplay.value) / 100;
+      } else {
+        if (resultDisplay.value) {
+          // add the operator on button click to the variable and save the display value to the variable
+          operator = value;
+          firstNum = Number(resultDisplay.value);
+          resultDisplay.value = "";
+        }
       }
-    } else if (value == "=") {
-      if (resultDisplay.value == "" || (operator == "/" && secondNum == 0)) {
-        resultDisplay.value = "Error";
-      } else if (firstNum !== "" && operator !== "") {
-        calculateResult();
-      }
-    } else if (value == "C") {
-      resultDisplay.value = "";
-      clearAll();
     } else {
-      if (resultDisplay.value == "0") {
+      // to add digit to the display
+      if (value == ".") {
+        if (
+          resultDisplay.value !== "" &&
+          !resultDisplay.value.includes(value)
+        ) {
+          resultDisplay.value += value;
+        }
+      } else if (resultDisplay.value == "0") {
+        resultDisplay.value = value;
+      } else {
+        resultDisplay.value += value;
+      }
+    }
+  });
+});
+
+window.addEventListener("keyup", (e) => {
+  if (e.key == "Backspace") {
+    resultDisplay.value = resultDisplay.value.slice(0, -1);
+  }
+  const buttons = document.querySelectorAll(`button[value="${e.key}"`);
+  buttons.forEach((button) => {
+    if (button.classList.contains("digit")) {
+      const value = button.value;
+      if (value == ".") {
+        if (
+          resultDisplay.value !== "" &&
+          !resultDisplay.value.includes(value)
+        ) {
+          resultDisplay.value += value;
+        }
+      } else if (resultDisplay.value == "0") {
         resultDisplay.value = value;
       } else {
         resultDisplay.value += value;
